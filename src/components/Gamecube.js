9aa1f178f6b2ede7e366a30ab1134429f4c3780f
clay <clay.nichols@gmail.com>
2023-02-22 01:17:37 -0600

    add all character assets

--- src/components/Gamecube.js
+++ src/components/Gamecube.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GamecubeImage from '../images/gamecube.jpg';
 import CharacterBar from '../components/CharacterBar';
+import Mario from '../images/characters/mario.jpg'
+import Monkey from '../images/characters/monkey.jpg'
+import Boo from '../images/characters/boo.jpeg'
 
 const Gamecube = () => {
+  const [charactersArray, setCharactersArray] = useState([
+    {name: 'Mario', source: Mario},
+    {name: 'Monkey', source: Monkey},
+    {name: 'Boo', source: Boo}
+  ])
+
   return (
     <div className="image-page gamecube">
-      <CharacterBar/>
+      <CharacterBar characters={charactersArray}/>
       <div className="image-page-container">
         <img src={GamecubeImage} className="main-image"/>
       </div>
--- src/components/Xbox.js
+++ src/components/Xbox.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import XboxImage from '../images/xbox.jpg';
 import CharacterBar from '../components/CharacterBar';
+import Beat from '../images/characters/beat.png'
+import Sam from '../images/characters/sam.png'
+import Raz from '../images/characters/raz.jpg'
 
 const Xbox = () => {
+  const [charactersArray, setCharactersArray] = useState([
+    {name: 'Beat', source: Beat},
+    {name: 'Sam', source: Sam},
+    {name: 'Raz', source: Raz}
+  ])
+
   return (
     <div className="image-page xbox"> 
-      <CharacterBar/>
+      <CharacterBar characters={charactersArray}/>
       <img src={XboxImage} className="main-image"/>
     </div>
   )
